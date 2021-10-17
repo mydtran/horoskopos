57a08983a4f8594efca68a1cd1009099b8e0c600
Diana <83438135+mydtran@users.noreply.github.com>
2021-10-17 09:48:09 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btnStarSign = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(184, 86)
+        Me.DateTimePicker1.MaxDate = New Date(2021, 12, 31, 0, 0, 0, 0)
+        Me.DateTimePicker1.MinDate = New Date(1950, 1, 1, 0, 0, 0, 0)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(243, 27)
+        Me.DateTimePicker1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(17, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(159, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Hi, what's your name?"
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.txtName.Location = New System.Drawing.Point(184, 39)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(243, 27)
+        Me.txtName.TabIndex = 2
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Controls.Add(Me.btnStarSign)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.Lavender
+        Me.GroupBox1.Location = New System.Drawing.Point(70, 46)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(452, 280)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "horoscope | horoskopos | ὡρόσκοπος"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.BackColor = System.Drawing.Color.White
+        Me.ListBox1.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.ListBox1.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(23, 176)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(404, 84)
+        Me.ListBox1.TabIndex = 6
+        Me.ListBox1.Tag = " "
+        '
+        'btnStarSign
+        '
+        Me.btnStarSign.BackColor = System.Drawing.Color.Lavender
+        Me.btnStarSign.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnStarSign.ForeColor = System.Drawing.Color.RoyalBlue
+        Me.btnStarSign.Location = New System.Drawing.Point(23, 130)
+        Me.btnStarSign.Name = "btnStarSign"
+        Me.btnStarSign.Size = New System.Drawing.Size(404, 35)
+        Me.btnStarSign.TabIndex = 5
+        Me.btnStarSign.Text = "Your Star Sign"
+        Me.btnStarSign.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(20, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(156, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Select your birth date"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(596, 400)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.Text = "horoskopos"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnStarSign As Button
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,195 @@
+﻿Public Class Form1
+    ReadOnly Old_StarSign() As String = {"Capricornus", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpius", "Sagittarius"}
+    '12 star signs
+    ReadOnly New_StarSign() As String = {"Capricornus", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpius", "Ophiuchus", "Sagittarius"}
+    '13 star signs
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub MonthCalendar1_DateChanged(sender As Object, e As DateRangeEventArgs)
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+        DateTimePicker1.Format = DateTimePickerFormat.Custom
+        DateTimePicker1.CustomFormat = "MMMM dd" 'Show me only the month and day... Years are overrated.
+    End Sub
+
+    Private Sub Label2_Click_1(sender As Object, e As EventArgs)
+
+    End Sub
+
+    '  Private Sub txtName_TextChanged(sender As Object, e As EventArgs) Handles txtName.TextChange
+    '  End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnStarSign.Click
+        Dim Month As String = Format(DateTimePicker1.Value, "MMMM")
+        Dim Day As Integer = Format(DateTimePicker1.Value, "dd")
+        Dim Year As Integer = Format(DateTimePicker1.Value, "yyyy")
+
+        Select Case Month
+            Case "January" 'Capricornus, Aquarius
+                If Day >= 1 And Day <= 19 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(0))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(1))
+                End If
+            Case "February" 'Aquarius, Pisces
+                If Day >= 1 And Day <= 18 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(1))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(2))
+                End If
+            Case "March" 'Pisces, Aries
+                If Day >= 1 And Day <= 20 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(2))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(3))
+                End If
+            Case "April" 'Aries, Taurus
+                If Day >= 1 And Day <= 19 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(3))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(4))
+                End If
+            Case "May" 'Taurus, Gemini
+                If Day >= 1 And Day <= 20 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(4))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(5))
+                End If
+            Case "June" 'Gemini, Cancer
+                If Day >= 1 And Day <= 20 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(5))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(6))
+                End If
+            Case "July" 'Cancer, Leo
+                If Day >= 1 And Day <= 22 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(6))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(7))
+                End If
+            Case "August" 'Leo, Virgo
+                If Day >= 1 And Day <= 22 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(7))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(8))
+                End If
+            Case "September" 'Virgo, Libra
+                If Day >= 1 And Day <= 22 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(8))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(9))
+                End If
+            Case "October" 'Libra, Scorpius
+                If Day >= 1 And Day <= 22 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(9))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(10))
+                End If
+            Case "November" 'Scorpius, Sagittarius
+                If Day >= 1 And Day <= 21 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(10))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(11))
+                End If
+            Case "December" 'Sagittarius, Capricornus
+                If Day >= 1 And Day <= 21 Then
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(11))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your traditional star sign is " & Old_StarSign(0))
+                End If
+        End Select
+        Select Case Month
+            Case "January" 'Sagittarius, Capricornus
+                If Day >= 1 And Day <= 18 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(12))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(0))
+                End If
+            Case "February" 'Capricornus, Aquarius
+                If Day >= 1 And Day <= 15 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(0))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(1))
+                End If
+            Case "March" 'Aquarius, Pisces
+                If Day >= 1 And Day <= 11 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(1))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(2))
+                End If
+            Case "April" 'Pisces, Aries
+                If Day >= 1 And Day <= 18 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(2))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(3))
+                End If
+            Case "May" 'Aries, Taurus
+                If Day >= 1 And Day <= 13 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(3))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(4))
+                End If
+            Case "June" 'Taurus, Gemini
+                If Day >= 1 And Day <= 19 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(4))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(5))
+                End If
+            Case "July" 'Gemini, Cancer
+                If Day >= 1 And Day <= 20 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(5))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(6))
+                End If
+            Case "August" 'Cancer, Leo
+                If Day >= 1 And Day <= 9 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(6))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(7))
+                End If
+            Case "September" 'Leo, Virgo
+                If Day >= 1 And Day <= 15 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(7))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(8))
+                End If
+            Case "October" 'Virgo, Libra
+                If Day >= 1 And Day <= 30 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(8))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(9))
+                End If
+            Case "November" 'Libra, Scorpius, Sagittarius
+                If Day >= 1 And Day <= 22 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(9))
+                ElseIf Day >= 23 And Day <= 29 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(10))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(11))
+                End If
+            Case "December" 'Sagittarius, Capricornus
+                If Day >= 1 And Day <= 17 Then
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(11))
+                Else
+                    ListBox1.Items.Add(txtName.Text & ", your actual star sign is " & New_StarSign(12))
+                End If
+        End Select
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
